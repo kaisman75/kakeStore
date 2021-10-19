db29a6fafaa316eda7b403406741c9c8baba309c
kaisman75 <kaismanai1975gmail.com>
2021-10-19 21:24:03 +0100

    ok

--- src/redux/reducers/product_Reducer.js
+++ src/redux/reducers/product_Reducer.js
@@ -1,18 +1,18 @@
 import { actionTypes } from "../contants/action_types";
-import { set_Products } from "../actions/productAction";
 
 
-const initialState= {
-products:[]
-}
+
+const initialState = {products:[]}
+
+ 
+    
 
 
-export const product_Reducer=(state=[],{type,payload})=>{
+
+export const product_Reducer=(state=initialState,{type,payload})=>{
     switch (type) {
         case actionTypes.SET_PRODUCTS:
-            return state={...state,products:payload};
-            
-    
+            return {...state, products:payload }
         default:
             return state;
     }
