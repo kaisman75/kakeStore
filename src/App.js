ec9aa09d845028c9258b305c81828e389b63cc5e
kais manai <kaismanai1975@gmail.com>
2021-10-22 12:54:46 +0100

    detail product

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Header from '../src/containers/Header';
 import './App.css';
 import ProductListing from './containers/product_listing';
 import ComponentProduct from './containers/componentProduct';
-import  Product_details from './containers/product_details'
+import Product_details from './containers/product_details'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
       <Router>
-         <Header/> 
+        <Header/> 
         <Switch>
             <Route path='/'exact component={ProductListing}/>
             <Route path='/Prod'exact component={ComponentProduct}/>
